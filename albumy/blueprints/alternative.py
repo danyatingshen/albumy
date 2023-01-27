606f97283163b4fc1b5f89bad9675650fa88bc0c
Amanda Shen <amandashen@amandas-mbp.wifi.local.cmu.edu>
2023-01-27 15:25:52 -0500

    task 2

--- albumy/blueprints/alternative.py
+++ albumy/blueprints/alternative.py
@@ -5,18 +5,27 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes, VisualFeatureTypes
 
 
-def get_alternative_text(local_path: str):
+def get_ml_fields(local_path: str):
     secrete = json.load(open('albumy/blueprints/secrete.json'))
     API_KEY = secrete['API_KEY']
     END_POINT = secrete['END_POINT']
     client = ComputerVisionClient(END_POINT, CognitiveServicesCredentials(API_KEY))
     try:
-        respond = client.describe_image_in_stream(
+        alt_respond = client.describe_image_in_stream(
             open(local_path, 'rb'),
             raw=True
         )
-        text = respond.output.captions[0].text
+        text = alt_respond.output.captions[0].text
+
+        tag_respond = client.tag_image_in_stream(
+            open(local_path, 'rb')
+        ).tags
+        tags = []
+        for tag in tag_respond:
+            tags.append(tag.name)
+
     except:
         print("Error occur when fetching alternating text from azure")
 
-    return text
+    return text, tags
+
